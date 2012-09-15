20d96a5b8356fae9e8b87b23eb51c13620ae8308
Cmaranec <cmaranec@seznam.cz>
2012-09-16 00:58:30 +0200

    Text can now have color :)

--- source/src/Elements.cpp
+++ source/src/Elements.cpp
@@ -1,6 +1,7 @@
 #include "Global.h"
 #include "Log.h"
 #include "Storage.h"
+#include "Parsers\StyleParser.h"
 #include "Defines\Slides.h"
 #include "Defines\Styles.h"
 #include "Defines\Effects.h"
@@ -23,7 +24,21 @@ void SlideElement::elemTextData::Draw(SlideElement* parent)
     {
         Style* myStyle = sStorage->GetStyle(parent->elemStyle.c_str());
 
+        // Set color if any
+        if (myStyle->fontColor)
+        {
+            uint32 color = (*(myStyle->fontColor));
+            glColor3ub(COLOR_R(color),COLOR_G(color),COLOR_B(color));
+        }
+
+        // draw text with own font. If not set, use default font
         if (myStyle->fontId >= 0)
             sSimplyFlat->Drawing->PrintText(myStyle->fontId, parent->typeText.position[0], parent->typeText.position[1], parent->typeText.text.c_str());
+        else
+            sSimplyFlat->Drawing->PrintText(sStorage->GetDefaultFontId(), parent->typeText.position[0], parent->typeText.position[1], parent->typeText.text.c_str());
+
+        // Set color back to white if necessary
+        if (myStyle->fontColor)
+            glColor3ub(255, 255, 255);
     }
 }
